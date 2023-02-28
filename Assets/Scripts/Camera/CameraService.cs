0660739390edf7d209a0b865f43eea7bf5e66542
Sergey <s.shishenya@gmail.com>
2023-02-28 14:39:51 +0400

    Создание контроллера камеры

---
+++ Assets/Scripts/Camera/CameraService.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraService : MonoBehaviour
+{
+    [SerializeField] private GameObject _generatorModel;
+    [SerializeField] private float _whellSpeed = 10f;
+    [SerializeField] private float _minZPosition = -10f;
+    [SerializeField] private float _maxZPosition = -2;
+    [SerializeField] private float _rotateSpeed = 1f;
+    private Transform _cameraTransform;
+
+    private void Awake()
+    {
+        _cameraTransform = Camera.main.transform;
+    }
+
+    private void Update()
+    {
+        ScrollCamera();
+        RotateGenerator();
+    }
+
+    /// <summary>
+    /// Приближение и отдаление
+    /// </summary>
+    private void ScrollCamera()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        if (scroll != 0.0f)
+        {
+            float posZ = Mathf.Clamp(_cameraTransform.position.z + (scroll * _whellSpeed), _minZPosition, _maxZPosition);
+            _cameraTransform.position = new Vector3(_cameraTransform.position.x, _cameraTransform.position.y, posZ);
+        }
+    }
+
+    /// <summary>
+    /// Поворот генератора и его частей
+    /// </summary>
+    private void RotateGenerator()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        if (horizontal!=0f)
+        {
+            _generatorModel.transform.Rotate(0, _rotateSpeed * horizontal, 0);
+        }
+    }
+
+}
